e5a977a1c4c9038ec2e691a52ba710c44e9803e6
Sunilkumar06 <sunilvj006@gmail.com>
2025-01-26 22:40:33 +0530

    Update player

--- IPL_Backend/IPL/src/main/java/com/IndainPremierLeague/IPL/player/PlayerConfig.java
+++ IPL_Backend/IPL/src/main/java/com/IndainPremierLeague/IPL/player/PlayerConfig.java
@@ -12,8 +12,8 @@ public class PlayerConfig {
     @Bean
     CommandLineRunner commandLineRunner(PlayerRepository playerRepository){
         return args -> {
-            Player Dhoni = new Player("MS Dhoni","India","Batsman","Right-Handed Middle Order",42,7,7777,7,"Chennai Super Kings");
-            Player Raina = new Player("Suresh Raina","India","Batsman","Left-Handed Top Order",38,3,3333,33,"Chennai Super Kings");
+            Player Dhoni = new Player("MS Dhoni","India","W-Batsman","Right-Handed Middle Order, Wicket Keeper",42,7,7777,7,"CSK");
+            Player Raina = new Player("Suresh Raina","India","Batsman","Left-Handed Top Order",38,3,3333,33,"CSK");
             playerRepository.saveAll(List.of(Dhoni, Raina));
             System.out.println("Success: Player Record Saved");
         };
